c5fc75968f20b14b8c06d3e079eac53f6f894678
Tushar Khairnar <tushar.khairnar@itariumtech.com>
2022-01-30 23:40:32 +0530

    Constant separation

--- src/main/java/com/services/blogrestapi/controller/PostController.java
+++ src/main/java/com/services/blogrestapi/controller/PostController.java
@@ -3,6 +3,7 @@ package com.services.blogrestapi.controller;
 import com.services.blogrestapi.payload.PostDTO;
 import com.services.blogrestapi.payload.PostResponse;
 import com.services.blogrestapi.service.PostService;
+import com.services.blogrestapi.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,36 +14,36 @@ public class PostController {
 
     private final PostService postService;
 
-    PostController(PostService postService){
+    PostController(PostService postService) {
         this.postService = postService;
     }
 
     @PostMapping
-    ResponseEntity createPost(@RequestBody PostDTO postDTO){
+    ResponseEntity createPost(@RequestBody PostDTO postDTO) {
         return new ResponseEntity<>(postService.createPost(postDTO), HttpStatus.CREATED);
     }
 
     @GetMapping
-    PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue="0", required = false) int pageNo,
-                             @RequestParam(value = "pageSize", defaultValue="10", required = false) int pageSize,
-                             @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-                             @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
-                             ){
+    PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+                             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+                             @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_FIELD, required = false) String sortBy,
+                             @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_ORDER, required = false) String sortDir
+    ) {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
     @GetMapping("/{id}")
-    ResponseEntity getPostById(@PathVariable Long id){
+    ResponseEntity getPostById(@PathVariable Long id) {
         return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    ResponseEntity updatePostById(@PathVariable Long id, @RequestBody PostDTO postDTO){
+    ResponseEntity updatePostById(@PathVariable Long id, @RequestBody PostDTO postDTO) {
         return new ResponseEntity<>(postService.updatePostById(id, postDTO), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    ResponseEntity deletePostById(@PathVariable Long id){
+    ResponseEntity deletePostById(@PathVariable Long id) {
         postService.deletePostById(id);
         return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
     }
---
+++ src/main/java/com/services/blogrestapi/utils/AppConstants.java
@@ -0,0 +1,11 @@
+package com.services.blogrestapi.utils;
+
+public class AppConstants {
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_ORDER = "asc";
+    public static final String DEFAULT_SORT_FIELD = "id";
+
+
+
+}
