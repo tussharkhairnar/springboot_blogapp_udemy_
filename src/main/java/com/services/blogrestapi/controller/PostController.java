1948f61593f93ab01e78a49b1c4ae76da637bbbc
Tushar Khairnar <tushar.khairnar@itariumtech.com>
2022-01-30 22:39:12 +0530

    Sorting
    Sort list by key in asc order

--- src/main/java/com/services/blogrestapi/controller/PostController.java
+++ src/main/java/com/services/blogrestapi/controller/PostController.java
@@ -24,8 +24,10 @@ public class PostController {
 
     @GetMapping
     PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue="0", required = false) int pageNo,
-                             @RequestParam(value = "pageSize", defaultValue="10", required = false) int pageSize){
-        return postService.getAllPosts(pageNo, pageSize);
+                             @RequestParam(value = "pageSize", defaultValue="10", required = false) int pageSize,
+                             @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy
+                             ){
+        return postService.getAllPosts(pageNo, pageSize, sortBy);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/services/blogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/services/blogrestapi/service/impl/PostServiceImpl.java
@@ -9,6 +9,7 @@ import com.services.blogrestapi.service.PostService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -44,9 +45,9 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize){
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy){
 
-        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
         Page<Post> posts =  postRepository.findAll(pageable);
         List<Post> listOfPosts = posts.getContent();
 
